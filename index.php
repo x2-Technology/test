56e9c9be4181a4b095029835e0750adaf941511a
root <root@localhost.localdomain>
2019-10-20 20:29:32 +0200

    index.php dosyasi güncellendi

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-echo phpinfo();
-echo "Süleyman Topaloglu";
-echo "developer@topaloglu.eu"
->
+	error_reporting(E_ALL);
+	echo phpinfo();
+	echo "Süleyman Topaloglu";
+	echo "developer@topaloglu.eu"
+?>
